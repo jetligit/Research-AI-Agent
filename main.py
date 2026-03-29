d5e3d835a83396e0b4974c38f77b6a0890fe7dbd
Jet Li <jel7426@g.rit.edu>
2026-03-29 01:16:39 -0400

    Formatted System Prompt for LLM

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 llm = ChatOpenAI(model = "gpt-4o-mini")
-response = llm.invoke("Why is the sky blue?")
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+         """
+        You are a research assistant that will help generate a research paper. 
+        Answer the user query and use necessary tools.
+        Wrap the output in this format and provide no other text\n{format_instructions} 
+        """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    
+    ]
+)
