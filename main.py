eef646e345963898bde8d8b7baf40eaebd5eb946
Jet Li <jel7426@g.rit.edu>
2026-04-03 22:54:12 -0400

    created search tools and save to file tool

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
@@ -21,7 +22,7 @@ prompt = ChatPromptTemplate.from_messages(
         ("system",
          """
         You are a research assistant that will help generate a research paper. 
-        Answer the user query and use necessary tools.
+        Answer the user query and use necessary tools. Save the data to a file.
         Wrap the output in this format and provide no other text\n{format_instructions} 
         """,
         ),
@@ -31,18 +32,20 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+tools=[search_tool, wiki_tool, save_tool]
 agent = create_tool_calling_agent(
     llm = llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
-raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
-print(raw_response)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can I help you research? ")
+raw_response = agent_executor.invoke({"query": query})
 
 try:
     structured_response = parser.parse(raw_response.get("output"))
+    print(structured_response)
 except Exception as e:
     print("Error Parsing Response", e)
 
